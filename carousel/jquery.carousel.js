db18971717554d61cad3962db45fcfebf92ea731
LetSevI <sudevgeny@gmail.com>
2018-10-05 14:09:42 +0300

    Changes

--- carousel/jquery.carousel.js
+++ carousel/jquery.carousel.js
@@ -1,11 +1,18 @@
 ;(function($) {
 
     var defaults = {
-        
+        items: 3,
+        loop: false,
+        auto: false,
+        vertical: false,
     };
 
     $.fn.carousel = function(options) {
+        var settings    = $.extend(true, {}, defaults, options),
+            $elem       = this,
+            $children   = $el.children();
 
+        
     };
 
 })(jQuery);
